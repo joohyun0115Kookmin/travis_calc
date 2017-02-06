5d81785e4b27e8f19d0a4df014532e6a23aa5384
Joohyun kyong <joohyun@kookmin.ac.kr>
2017-02-07 04:26:19 +0900

    initial commit

---
+++ src/main/java/com/sungshin/Calc.java
@@ -0,0 +1,14 @@
+package com.sungshin;
+
+public class Calc {
+    public int add(int x, int y) {
+        System.out.println(“add func”);
+        return x+y;
+    }
+
+    public int sub(int x, int y) {
+        return x+y;
+    }
+
+}
+
